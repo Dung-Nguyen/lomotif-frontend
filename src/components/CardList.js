ea9cef7f90c3fcfea17ba4e46cbefd36b18be917
Chris Nguyen <chris.dungnguyen@gmail.com>
2019-02-12 23:08:01 +0700

    Add config api axios

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../redux/actions/card.action'
+import Loading from './Loading'
 
-const mapStateToProps = state => {
-  return {}
-}
+const mapStateToProps = state => ({
+  ...state,
+  cards: state.cards
+})
 
-const mapDispatchToProps = dispatch => {
-  return {}
-}
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ ...actions }, dispatch)
+})
 
 const cardArr = [
   {
@@ -23,12 +28,23 @@ const cardArr = [
 ]
 
 class CardList extends Component {
+  componentDidMount = () => {
+    const { getCard } = this.props.actions
+    getCard()
+  }
+
   render() {
+    const { cards } = this.props
+    if (cards.hasOwnProperty('payload') && cards.payload.getCardPending) {
+      console.log('asdas')
+      return <Loading />
+    }
+
     return (
       <div className="card__list">
         <ul>
           {cardArr.map(item => (
-            <li>
+            <li key={item.dbf_id}>
               <p>{item.dbf_id}</p>
               <p>{item.name}</p>
               <p>{item.player_class}</p>
@@ -39,7 +55,10 @@ class CardList extends Component {
     )
   }
 }
-
+CardList.propTypes = {
+  cards: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+}
 export default connect(
   mapStateToProps,
   mapDispatchToProps
--- src/redux/reducers/card.reducer.js
+++ src/redux/reducers/card.reducer.js
@@ -1,9 +1,14 @@
-import { FETCH_CARD } from '../constants'
+import { GET_CARD_SUCCESS, GET_CARD_BEGIN, GET_CARD_ERROR } from '../constants'
 
-const postReducer = (state = [], action) => {
+const postReducer = (state = {}, action) => {
+  const payload = action.payload
   switch (action.type) {
-    case FETCH_CARD:
-      return [...state, action.payload]
+    case GET_CARD_BEGIN:
+      return { ...state, payload }
+    case GET_CARD_SUCCESS:
+      return { ...state, payload }
+    case GET_CARD_ERROR:
+      return { ...state, payload }
     default:
       return state
   }
