733aa517573fe53c5b34ee214c0e0fab1db696c2
ayanna1211 <minhthaon2@gmail.com>
2019-02-13 18:11:46 +0700

    style grid

--- src/App.js
+++ src/App.js
@@ -17,11 +17,20 @@ class App extends Component {
   render() {
     console.log(this.props)
     return (
-      <div className="container">
-        <h1>Hearthstone Deck</h1>
-        <div>
-          Content Card here
-          <CardList />
+      <div className="body">
+        <div className="b-header">
+          <div className="container">
+          <div className="header-inner">
+            <h1 className="branch-name">Hearthstone Deck</h1>
+          </div>
+          </div>
+        </div>
+        
+        <div className="b-main">
+          <div className="container">
+            <h2 className="title-main">Content Card here</h2>
+            <CardList />
+          </div>
         </div>
       </div>
     )
